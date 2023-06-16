125d46d3bc975f0c0fc3263045c5ee4b4cd67dba
Sangamesh30 <sangamesh.3094@gmail.com>
2023-06-16 18:46:06 +0530

    playbook

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
   default = [
-    {name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.awsdevops73.store", type = "string" },
-    {name = "roboshop.dev.frontend.user_url", value = "htts://user-dev.awsdevops73.store", type = "string" },
-    {name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.awsdevops73.store", type = "string" },
-    {name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.awsdevops73.store", type = "string" },
-    {name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.awsdevops73.store", type = "string" }
+    {name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.awsdevops73.store", type = "String" },
+    {name = "roboshop.dev.frontend.user_url", value = "htts://user-dev.awsdevops73.store", type = "String" },
+    {name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.awsdevops73.store", type = "String" },
+    {name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.awsdevops73.store", type = "String" },
+    {name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.awsdevops73.store", type = "String" }
     ]
 }
